7ec46862b4082670bfd23dbaeb2e0978bc7a8e6a
MaksimMakhmutov <makhmutov.mv@yandex.ru>
2025-07-21 20:46:01 +0700

    Frontend 01

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { AdminPage, ProductPage, HomePage, DetailPage } from './pages';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  AdminPage,
+  ProductPage,
+  HomePage,
+  DetailPage,
+  CartPage,
+} from './pages';
 import './App.css'; // Импортируем стили
+import { Navbar } from './components';
 
 const App = () => {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Главная</Link>
-            </li>
-            <li>
-              <Link to="/products">Товары</Link>
-            </li>
-            <li>
-              <Link to="/admin">Админ-панель</Link>
-            </li>
-          </ul>
-        </nav>
+        <Navbar />
         <Routes>
+          <Route path="/cart" element={<CartPage />} />
           <Route path="/" element={<HomePage />} />
           <Route path="/products" element={<ProductPage />} />
           <Route path="/admin" element={<AdminPage />} />
--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,13 +1,14 @@
+// src/components/ProductCard.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const ProductCard = ({ product }) => {
+export const ProductCard = ({ id, image, name, price }) => {
   return (
     <div className="product-card">
-      <Link to={`/products/${product.id}`}>
-        <img src={product.image} alt={product.name} style={{ width: '100%' }} />
-        <h3>{product.name}</h3>
-        <p>Цена: {product.price} $</p>
+      <Link to={`/products/${id}`}>
+        <img src={image} alt={name} style={{ width: '100%' }} />
+        <h3>{name}</h3>
+        <p>Цена: {price} $</p>
       </Link>
     </div>
   );
